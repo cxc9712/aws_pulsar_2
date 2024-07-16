b1cfea50ccf12f370cf573409c3e9052abfcb7d3
xche0037 <xche0037@student.monash.edu>
2024-07-16 14:06:20 +1000

    2nd commit

--- producer.py
+++ producer.py
@@ -1,3 +1,20 @@
 ##Producer1
-import pulsar
+
 import csv
+import pulsar
+# Initialize the Pulsar client
+client = pulsar.Client('pulsar://localhost:6650')
+
+# Create a producer on a specific topic
+producer = client.create_producer('my-topic')
+
+# Read data from a CSV file and send it to the Pulsar topic
+with open('cir_sample.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        message = ','.join(row)
+        producer.send(message.encode('utf-8'))
+
+# Close the producer and the client
+# producer.close()
+client.close()
